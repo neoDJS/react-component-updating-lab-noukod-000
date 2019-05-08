5410429ea7616c1d87e6128f48ec7ec724757201
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-08 07:05:39 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-class Timer extends React.PureComponent {
+class Timer extends PureComponent {
 
   constructor() {
     super()
